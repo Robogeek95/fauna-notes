29228e6984c2970e72e837c626fb2cfecac4401a
robogeek95 <azeezlukman95@gmail.com>
2021-05-27 00:44:06 +0100

    implemented auth screens

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  primary: "#002754",
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+function App({ Component }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
---
+++ pages/signup.js
@@ -0,0 +1,119 @@
+import { Button } from "@chakra-ui/button";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Box, Flex, Stack } from "@chakra-ui/layout";
+import { Field, Form, Formik } from "formik";
+import { useRouter } from "next/router";
+import React from "react";
+
+export default function Signup() {
+  const router = useRouter();
+  function validateName(value) {
+    let error;
+    if (!value) {
+      error = "Email is required";
+    } else if (value.toLowerCase().length < 3) {
+      error = "too short";
+    }
+    return error;
+  }
+
+  function validateEmail(value) {
+    let error;
+    if (!value) {
+      error = "Email is required";
+    } else if (value.toLowerCase().length < 3) {
+      error = "too short";
+    }
+    return error;
+  }
+
+  function validatePassword(value) {
+    let error;
+    if (!value) {
+      error = "Password is required";
+    } else if (value.toLowerCase().length < 3) {
+      error = "too short";
+    }
+    return error;
+  }
+
+  function submit(values, actions) {
+    setTimeout(() => {
+      actions.setSubmitting(false);
+      router.push("/");
+    }, 1000);
+  }
+
+  return (
+    <>
+      <Flex justifyContent="center" py={10}>
+        <Box w="30%" mt={12}>
+          <Formik initialValues={{ email: "", password: "" }} onSubmit={submit}>
+            {(formProps) => (
+              <Form>
+                <Stack spacing={3}>
+                  <Field name="name" validate={validateName}>
+                    {({ field, form }) => (
+                      <FormControl
+                        id="name"
+                        isInvalid={form.errors.name && form.touched.name}
+                      >
+                        <FormLabel>Name</FormLabel>
+                        <Input {...field} type="name" variant="filled" />
+                        <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+
+                  <Field name="email" validate={validateEmail}>
+                    {({ field, form }) => (
+                      <FormControl
+                        id="email"
+                        isInvalid={form.errors.email && form.touched.email}
+                      >
+                        <FormLabel>Email address</FormLabel>
+                        <Input {...field} type="email" variant="filled" />
+                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+
+                  <Field name="password" validate={validatePassword}>
+                    {({ field, form }) => (
+                      <FormControl
+                        id="password"
+                        isInvalid={
+                          form.errors.password && form.touched.password
+                        }
+                      >
+                        <FormLabel>Password</FormLabel>
+                        <Input {...field} type="password" variant="filled" />
+                        <FormErrorMessage>
+                          {form.errors.password}
+                        </FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+
+                  <Button
+                    mt={4}
+                    colorScheme="blue"
+                    isLoading={formProps.isSubmitting}
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Flex>
+    </>
+  );
+}
